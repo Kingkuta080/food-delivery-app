eefb75a03abaf91a4e670f2de0665a46f6f8ecd8
kingkuta080 <alubaidullahi080@gmail.com>
2026-01-13 13:16:42 +0100

    Implement logout functionality: Added a confirmation modal for logout actions and updated navigation logic in the ProfileDrawer for improved user experience.

---
+++ app/(screens)/NotificationTap.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const NotificationTap = () => {
+  return (
+    <View>
+      <Text>NotificationTap</Text>
+    </View>
+  )
+}
+
+export default NotificationTap
+
+const styles = StyleSheet.create({})
